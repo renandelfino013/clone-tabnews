55ba304db54805e6ac0df9dff5df1fddf038beb0
renandelfino013 <renancontaps@gmail.com>
2026-06-27 21:36:14 -0300

    local db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(req, resp) {
+import { query } from "../../../../infra/database";
+async function status(req, resp) {
+  const result = await query("select 1 + 1;");
+  console.log(result[0]);
   return resp.status(200).json({ status: "ok" });
 }
 
