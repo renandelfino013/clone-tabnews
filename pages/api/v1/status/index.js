787a1e8af16bae77d374fd3bd2fcfd7b48e53940
renandelfino013 <renancontaps@gmail.com>
2026-06-28 15:56:06 -0300

    add jsconfig for disable relative paths

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import { query } from "../../../../infra/database";
+import { query } from "infra/database";
 async function status(req, resp) {
   const result = await query("select 1 + 1;");
   console.log(result[0]);
